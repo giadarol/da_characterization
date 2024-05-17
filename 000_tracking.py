cad4c27a3f78734fca50fea83c6485e9870f9aea
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2024-05-17 10:19:29 +0200

    Another case

--- 000_tracking.py
+++ 000_tracking.py
@@ -6,7 +6,7 @@ import numpy as np
 
 collider = xt.Multiline.from_json('collider_04_tuned_and_leveled_bb_on.json')
 
-collider.vars['beambeam_scale'] = 0.5
+collider.vars['beambeam_scale'] = 0.4
 
 line = collider.lhcb1
 
@@ -54,7 +54,7 @@ for shift in shift_r_vector:
 all_particles = xt.Particles.merge(particles_objects)
 all_particles_init = all_particles.copy(_context=xo.context_default)
 
-context = xo.ContextCupy(device=1)
+context = xo.ContextCupy(device=2)
 line.discard_tracker()
 line.build_tracker(_context=context)
 all_particles.move(_context=context)
@@ -71,5 +71,5 @@ dct_out = {
     'all_particles': all_particles.to_dict()}
 
 import json
-with open('out_1M_bb_0.5.json', 'w') as fid:
+with open('out_1M_bb_0.4.json', 'w') as fid:
     json.dump(dct_out, fid, cls=xo.JEncoder)
