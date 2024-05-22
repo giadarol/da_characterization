d9319e193900a3d9c8c13d55859ef814e11c0b23
giadarol <giovanni.iadarola@cern.ch>
2024-05-22 21:35:35 +0200

    Amplitude detuning coeff

--- 005_simulation_fingerprint.py
+++ 005_simulation_fingerprint.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import numpy as np
 
 collider = xt.Multiline.from_json('collider_04_tuned_and_leveled_bb_on.json')
 
@@ -10,6 +11,14 @@ line.vars['beambeam_scale'] = bb_scale
 
 tw = line.twiss()
 tt = line.get_table()
+
+det = line.get_amplitude_detuning_coefficients(
+    a0_sigmas=0.1, a1_sigmas=0.2, a2_sigmas=0.3)
+
+det_table = xt.Table({
+    'name': np.array(list(det.keys())),
+    'value': np.array([v for v in det.values()]),
+})
 
 out = ''
 
@@ -23,6 +32,7 @@ out += '\n'
 
 out += f'Tunes:        Qx  = {tw["qx"]:.5f}       Qy = {tw["qy"]:.5f}\n'
 out += f"Chromaticity: Q'x = " + f'{tw["dqx"]:.2f}     ' + "Q'y = " + f'{tw["dqy"]:.2f}\n'
+out += f'c_minus:      {tw["c_minus"]:.5e}\n'
 out += '\n'
 
 out += f'Synchrotron tune: {tw["qs"]:5e}\n'
@@ -34,17 +44,20 @@ out += f'Twiss parameters and phases at IPs:\n'
 out += tw.rows['ip.*'].cols[
     'name s betx bety alfx alfy mux muy'].show(
         output=str, max_col_width=int(1e6), digits=8)
-out += '\n'
+out += '\n\n'
 
 out += f'Dispersion at IPs:\n'
 out += tw.rows['ip.*'].cols[
     'name s dx dy dpx dpy'].show(
         output=str, max_col_width=int(1e6), digits=8)
-out += '\n'
+out += '\n\n'
 
 out += 'Crab dispersion at IPs:\n'
 out += tw.rows['ip.*'].cols[
     'name s dx_zeta dy_zeta dpx_zeta dpy_zeta'].show(
         output=str, max_col_width=int(1e6), digits=8)
-out += '\n'
+out += '\n\n'
+
+out += 'Amplitude detuning coefficients:\n'
+out += det_table.show(output=str, max_col_width=int(1e6), digits=6)
 
